4070402e02ddd385a9674580ad17eb11ee36fc35
GreenWizard <siZeOf.2011@gmail.com>
2017-12-16 01:20:21 +0200

    Just play around

--- src/org/trueoop/app/Main.java
+++ src/org/trueoop/app/Main.java
@@ -1,9 +1,23 @@
 package org.trueoop.app;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		IApplication app = new Application();
-		app.run(null);
+		IApplication app = new Application(
+			new FormattedText(
+				"A = %d, B = %d",
+				new TypedInt("Input A:"),
+				new TypedInt("Input B:")
+			)
+		);
+		
+		app.run(
+			new Enviroment(
+				new Input(new Scanner(System.in)), 
+				new Output(System.out)
+			)
+		);
 	}
 }
