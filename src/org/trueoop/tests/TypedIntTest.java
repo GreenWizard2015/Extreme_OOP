1921c1ec6b0f27f85d3575e13ec6752bf7c50933
GreenWizard <siZeOf.2011@gmail.com>
2017-12-16 00:59:45 +0200

    Title for TypedInt

--- src/org/trueoop/tests/TypedIntTest.java
+++ src/org/trueoop/tests/TypedIntTest.java
@@ -6,7 +6,9 @@ import static org.mockito.Mockito.*;
 import org.junit.Test;
 import org.trueoop.app.IEnviroment;
 import org.trueoop.app.IInput;
+import org.trueoop.app.IOutput;
 import org.trueoop.app.IValue;
+import org.trueoop.app.Output;
 import org.trueoop.app.TypedInt;
 
 public class TypedIntTest {
@@ -24,5 +26,20 @@ public class TypedIntTest {
 		assertEquals(val.value(env), correctValue);
 		
 		verify(input, times(1)).Int();
+	}
+	
+	@Test
+	public void printTitle() {
+		final String title = "title for input";
+		
+		IOutput output = mock(IOutput.class);
+		IEnviroment env = mock(IEnviroment.class);
+		when(env.input()).thenReturn(mock(IInput.class));
+		when(env.output()).thenReturn(output);
+		
+		IValue<Integer> val = new TypedInt(title);
+		val.value(env);
+		
+		verify(output, times(1)).println(title);
 	}
 }
