6786e972b028da75cf2125f7ef81c576300b0179
GreenWizard <siZeOf.2011@gmail.com>
2017-12-16 19:22:09 +0200

    Build actually programm.

--- src/org/trueoop/app/Main.java
+++ src/org/trueoop/app/Main.java
@@ -5,13 +5,23 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		IApplication app = new Application(
-			new FormattedText(
-				"A = %d, B = %d",
-				new TypedInt("Input A:"),
-				new TypedInt("Input B:")
-			)
-		);
+		IApplication app = 
+			new Application(
+				new FormattedText(
+					"Estimation of pi = %1.5f (actually is 3.141592)",
+					new Estimator(
+						new Trial(
+							new Dartsboard(
+								new HitPoints()
+							),
+							new Cached<>(
+								new TypedInt("How many times should the dart be thrown per trial?")
+							)
+						),
+						new TypedInt("How many trials do you want to simulate?")
+					)
+				)
+			);
 		
 		app.run(
 			new Enviroment(
