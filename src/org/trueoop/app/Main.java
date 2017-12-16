dcba5fe57f5f15ac6fe8424cf240a0ceaab2e7b9
GreenWizard <siZeOf.2011@gmail.com>
2017-12-16 21:37:29 +0200

    Some more fun

--- src/org/trueoop/app/Main.java
+++ src/org/trueoop/app/Main.java
@@ -5,20 +5,29 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		IValue<Double> Pi =
+			new Cached<>(
+				new Estimator(
+					new Trial(
+						new Dartsboard(
+							new HitPoints()
+						),
+						new Cached<>(
+							new TypedInt("How many times should the dart be thrown per trial?")
+						)
+					),
+					new TypedInt("How many trials do you want to simulate?")
+				)
+			);
+			
 		IApplication app = 
 			new Application(
 				new FormattedText(
-					"Estimation of pi = %1.5f (actually is 3.141592)",
-					new Estimator(
-						new Trial(
-							new Dartsboard(
-								new HitPoints()
-							),
-							new Cached<>(
-								new TypedInt("How many times should the dart be thrown per trial?")
-							)
-						),
-						new TypedInt("How many trials do you want to simulate?")
+					"Estimation of pi = %1.5f (actually is 3.141592, diff = %.5f)",
+					Pi,
+					new Transformed<>(
+						x -> Math.abs(x - 3.141592),
+						Pi
 					)
 				)
 			);
